8a2251c802f1795d85c78ddd46771e4a81a0b4e6
Gravity <alternativegravitational@protonmail.com>
2024-11-24 13:47:48 +0800

    chiri pee

--- home.nix
+++ home.nix
@@ -23,23 +23,25 @@
   services.syncthing = {
     enable = true;
     settings = {
-      options = {
-        globalAnnounceEnabled = false;
-      };
       devices = {
         phone = {
+          allowedNetworks = "192.168.0.0/24";
           id = "U2WXU7A-PJ7IO6X-H6YCEXI-SIBCKKW-M7XAOBK-EW5IIIL-3CGARBS-G2LOJA2";
-          autoAcceptFolders = true;
+          introducer = true;
         };
         clear = {
+          allowedNetworks = "192.168.0.0/24";
           id = "4L66SSN-RBMZE3X-IOJNHJD-UT26UQV-K4HENC2-GQ3QARP-JHQTKWX-EQZZSA3";
-          autoAcceptFolders = true;
+          introducer = true;
         };
       };
       folders = {
         "/home/gravity/Notes" = {
-          id = "syncme";
-          devices = [ "clear" "phone" ];
+          id = "i2ekx-2lgrg";
+          devices = [
+            "clear"
+            "phone"
+          ];
           label = "Notes";
         };
       };
