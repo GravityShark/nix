1b014c41eae0dcf48c4451b0a62ea8cb96f7baa6
Gravity <alternativegravitational@protonmail.com>
2024-11-30 07:55:54 +0800

    Update configuration

--- dump/.config/fish/functions/hms.fish
+++
@@ -1,4 +0,0 @@
-function hms --wraps=home-manager\ switch\ --flake\ \~/.nix\\\?submodules=1 --description alias\ hms\ home-manager\ switch\ --flake\ \~/.nix\\\?submodules=1
-  home-manager switch --flake ~/.nix\?submodules=1 $argv
-        
-end
--- dump/.config/fish/functions/nre.fish
+++
@@ -1,4 +0,0 @@
-function nre --wraps='nvim ~/.nix && nixfmt ~/.nix/*' --description 'alias nre nvim ~/.nix && nixfmt ~/.nix/*'
-  $EDITOR ~/.nix/configuration.nix && nixfmt ~/.nix/* $argv
-        
-end
