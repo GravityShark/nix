08258822853f6f11107067fcc865918cbfd43aa2
Gravity <alternativegravitational@protonmail.com>
2024-12-11 06:28:57 +0800

    Update configuration

--- home/desktop.nix
+++ home/desktop.nix
@@ -2,54 +2,56 @@
 
 {
   xdg.desktopEntries = {
-    firefox = {
-      name = "Firefox";
-      genericName = "Web Browser";
-      exec = "firefox %U";
-      terminal = false;
-      categories = [
-        "Application"
-        "Network"
-        "WebBrowser"
-      ];
-      mimeType = [
-        "text/html"
-        "text/xml"
-      ];
-    };
-    # emacsclient = {
-    #   name = "Emacs (Client)";
-    #   genericName = "Text Editor";
-    #   comment = "Edit text";
-    #   exec = "$(which emacsclient) --create-frame --alternate-editor=$(which emacs) $@ ";
-    #   icon = "emacs";
+    # firefox = {
+    #   name = "Firefox";
+    #   genericName = "Web Browser";
+    #   exec = "firefox %U";
     #   terminal = false;
-    #   type = "Application";
     #   categories = [
-    #     "Utility"
-    #     "TextEditor"
+    #     "Application"
+    #     "Network"
+    #     "WebBrowser"
     #   ];
-    #   startupNotify = true;
-    #   startupWMClass = "Emacs";
-    #   keywords = "emacsclient";
-    #
     #   mimeType = [
-    #     "text/english"
-    #     "text/plain"
-    #     "text/x-makefile"
-    #     "text/x-c++hdr"
-    #     "text/x-c++src"
-    #     "text/x-chdr"
-    #     "text/x-csrc"
-    #     "text/x-java"
-    #     "text/x-moc"
-    #     "text/x-pascal"
-    #     "text/x-tcl"
-    #     "text/x-tex"
-    #     "application/x-shellscript"
-    #     "text/x-c"
-    #     "text/x-c++"
+    #     "text/html"
+    #     "text/xml"
     #   ];
     # };
+    emacsclient = {
+      name = "Emacs (Client)";
+      genericName = "Text Editor";
+      comment = "Edit text";
+      exec = "emacsclient --create-frame --alternate-editor=emacs %F";
+      icon = "emacs";
+      terminal = false;
+      type = "Application";
+      categories = [
+        "Utility"
+        "TextEditor"
+      ];
+      startupNotify = true;
+      # settings = {
+      #   startupWMClass = "Emacs";
+      #   keywords = "emacsclient";
+      # };
+
+      mimeType = [
+        "text/english"
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-java"
+        "text/x-moc"
+        "text/x-pascal"
+        "text/x-tcl"
+        "text/x-tex"
+        "application/x-shellscript"
+        "text/x-c"
+        "text/x-c++"
+      ];
+    };
   };
 }
