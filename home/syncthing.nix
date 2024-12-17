248eee063164868f1cd4e2ec0179d636ec598880
Gravity <alternativegravitational@protonmail.com>
2024-12-17 15:26:47 +0800

    Update configuration

--- home/syncthing.nix
+++ home/syncthing.nix
@@ -7,12 +7,14 @@
     settings = {
       devices = {
         phone = {
-          allowedNetworks = "192.168.0.0/24";
+          name = "phone";
+          allowedNetwork = "192.168.0.0/24";
           id = "U2WXU7A-PJ7IO6X-H6YCEXI-SIBCKKW-M7XAOBK-EW5IIIL-3CGARBS-G2LOJA2";
           introducer = true;
         };
         clear = {
-          allowedNetworks = "192.168.0.0/24";
+          name = "clear";
+          allowedNetwork = "192.168.0.0/24";
           id = "4L66SSN-RBMZE3X-IOJNHJD-UT26UQV-K4HENC2-GQ3QARP-JHQTKWX-EQZZSA3";
           introducer = true;
         };
