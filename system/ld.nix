6e8220252dd3720d7719541f42262c8ee593ae21
Gravity <alternativegravitational@protonmail.com>
2025-01-25 11:49:46 +0800

    Update configuration

--- system/ld.nix
+++ system/ld.nix
@@ -2,6 +2,7 @@
 {
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
+    gcc_multi
     # Add any missing dynamic libraries for unpackaged programs
     # here, NOT in environment.systemPackages
   ];
