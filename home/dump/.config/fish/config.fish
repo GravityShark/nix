da20f43f0ed0ba25cb7c0222528a66dba42407ae
Gravity <alternativegravitational@protonmail.com>
2025-02-22 10:25:42 +0800

    Update configuration

--- home/dump/.config/fish/config.fish
+++ home/dump/.config/fish/config.fish
@@ -66,6 +66,7 @@ if status is-interactive
     set --export plugins https\x3a//github\x2ecom/kidonng/plug\x2efish https\x3a//github\x2ecom/franciscolourenco/done https\x3a//github\x2ecom/nickeb96/puffer\x2dfish https\x3a//github\x2ecom/GravityShark0/safe-rm
     set CFLAGS '-std=c99 -Wall -Werror'
 
+    alias s sesh
     if not source $__fish_user_data_dir/plugins/plug.fish/conf.d/plugin_load.fish
         echo -e "plug.fish doesn't seem to exist, trying to install"
         curl -L https://github.com/kidonng/plug.fish/raw/v3/conf.d/plugin_load.fish | source
