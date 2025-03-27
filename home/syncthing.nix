410ec6ad83a0427bca611c85af452ef74cce6afd
Gravity <alternativegravitational@protonmail.com>
2025-03-28 06:14:39 +0800

    Update configuration

--- home/syncthing.nix
+++ home/syncthing.nix
@@ -12,6 +12,12 @@
           id = "U2WXU7A-PJ7IO6X-H6YCEXI-SIBCKKW-M7XAOBK-EW5IIIL-3CGARBS-G2LOJA2";
           introducer = true;
         };
+        brick = {
+          name = "brick";
+          allowedNetwork = "192.168.0.0/24";
+          id = "QFP3772-B63OTA4-WXRQQ47-7VWCIDL-4Q6CMI7-ODY6RVA-JBXB5EJ-FXAWFQ5";
+          introducer = true;
+        };
         clear = {
           name = "clear";
           allowedNetwork = "192.168.0.0/24";
@@ -25,6 +31,7 @@
           devices = [
             "clear"
             "phone"
+            "brick"
           ];
           label = "Notes";
           versioning.type = "simple";
