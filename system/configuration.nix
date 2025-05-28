ec58478bd97c1c52626877a6321429614790813d
Gravity <alternativegravitational@protonmail.com>
2025-05-28 17:12:54 +0800

    Update configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -16,7 +16,7 @@
   nixpkgs.config.allowUnfreePredicate =
     pkg:
     builtins.elem (lib.getName pkg) [
-      "discord-canary"
+      # "discord-canary"
       "zoom"
       # "zerotierone"
       # "nvidia-x11"
