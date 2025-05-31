f1ab25029901431347c70e46762b4cec1fdc6f85
Gravity <alternativegravitational@protonmail.com>
2025-05-31 19:42:09 +0800

    Update configuration

--- system/gnome.nix
+++ system/gnome.nix
@@ -1,11 +1,9 @@
 { lib, pkgs, ... }:
 
 {
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Enables gnome and gdm
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   # Remove xterm
   services.xserver.excludePackages = [ pkgs.xterm ];
 
