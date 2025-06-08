de5613bd27d6de98b9a1911f18efd39d64a70657
Gravity <alternativegravitational@protonmail.com>
2025-06-08 12:57:48 +0800

    Update configuration

--- home/packages/cli.nix
+++ home/packages/cli.nix
@@ -1,30 +1,34 @@
 { pkgs }:
 
+let
+  tex = (
+    pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-basic
+        dvisvgm
+        dvipng # for preview and export as html
+        wrapfig
+        amsmath
+        ulem
+        hyperref
+        capt-of
+        etoolbox
+        nopageno
+        mlmodern
+        metafont
+        ;
+      #(setq org-latex-compiler "lualatex")
+      #(setq org-preview-latex-default-process 'dvisvgm)
+    }
+  );
+in
+
 with pkgs;
 [
   # CLIs
   nix-your-shell
   sesh
+  tex
   tomato-c
   xdg-terminal-exec
-
-  # texlive.combine
-  # {
-  #   inherit (pkgs.texlive)
-  #     scheme-basic
-  #     dvisvgm
-  #     dvipng # for preview and export as html
-  #     wrapfig
-  #     amsmath
-  #     ulem
-  #     hyperref
-  #     capt-of
-  #     etoolbox
-  #     nopageno
-  #     mlmodern
-  #     metafont
-  #     ;
-  #   #(setq org-latex-compiler "lualatex")
-  #   #(setq org-preview-latex-default-process 'dvisvgm)
-  # }
 ]
