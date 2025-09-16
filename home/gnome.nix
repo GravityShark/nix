b8a5d9d38384ad380298cdf8bf059bf227493782
Gravity <alternativegravitational@protonmail.com>
2025-09-16 11:08:57 +0800

    Update configuration

--- home/gnome.nix
+++ home/gnome.nix
@@ -20,7 +20,7 @@
   # Gnome settings
 
   dconf.settings = {
-
+    "org/gnome/desktop/sound".allow-volume-above-100-percent = true;
     "org/gnome/desktop/session".idle-delay = 300;
     "org/gnome/desktop/peripherals/mouse".accel-profile = "flat";
     "org/gnome/desktop/peripherals/mouse".speed = -0.6;
