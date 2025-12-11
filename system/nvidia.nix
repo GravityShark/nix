08a65f4238d1e14b792971108e289c4c23b01ff3
Gravity <alternativegravitational@protonmail.com>
2025-12-11 19:07:05 +0800

    Update configuration

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -48,9 +48,11 @@
     ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
 
     # Enable runtime PM for NVIDIA VGA/3D controller devices on adding device
-    # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-    # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
   '';
+  # systemd.tmpfiles.rules = [ "w /sys/bus/pci/devices/0000:02:00.0/power/control - - - - auto" ];
+
   boot.extraModprobeConfig = ''
     options nvidia "NVreg_DynamicPowerManagement=0x02"
     options nvidia "NVreg_EnableGpuFirmware=0"
