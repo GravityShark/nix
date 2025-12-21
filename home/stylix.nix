1769343fc8d0d968d6a055a3cb24e39c4bb8e046
Gravity <alternativegravitational@protonmail.com>
2025-12-21 22:20:03 +0800

    Update configuration

--- home/stylix.nix
+++ home/stylix.nix
@@ -4,10 +4,10 @@
   # https://nix-community.github.io/stylix/configuration.html
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-light-hard.yaml";
-  # stylix.image = pkgs.fetchurl {
-  #   url = "https://gruvbox-wallpapers.pages.dev/wallpapers/minimalistic/light/Tranquility.png";
-  #   hash = "sha256-c9bf91fe51fc39a41c345214758d2a9459ac37ab32d46297855ecbc9a40751ed";
-  # };
+  stylix.image = pkgs.fetchurl {
+    url = "https://gruvbox-wallpapers.pages.dev/wallpapers/minimalistic/light/Tranquility.png";
+    hash = "c9bf91fe51fc39a41c345214758d2a9459ac37ab32d46297855ecbc9a40751ed";
+  };
   stylix.fonts = {
     serif = {
       package = pkgs.aporetic;
