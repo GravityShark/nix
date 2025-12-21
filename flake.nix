9d187e70c0ec88901d4eaf42cb352753df457544
Gravity <alternativegravitational@protonmail.com>
2025-12-21 22:18:08 +0800

    Update configuration

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:nix-community/stylix";
+      url = "github:nix-community/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser-flake = {
