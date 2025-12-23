3c73b6bfc6aa168b78e376d5e0743b8203211e97
Gravity <alternativegravitational@protonmail.com>
2025-12-23 22:28:12 +0800

    Update configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -56,7 +56,7 @@
     "nix-command"
     "flakes"
   ];
-  nix.settings.extra-substitutors = [ "https://nix-community.cachix.org" ];
+  nix.settings.extra-substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
