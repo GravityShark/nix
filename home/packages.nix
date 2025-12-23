9f076e960662ff43e827701434358f6033c81ab7
Gravity <alternativegravitational@protonmail.com>
2025-12-23 15:28:49 +0800

    Update configuration

--- home/packages.nix
+++ home/packages.nix
@@ -26,10 +26,10 @@ in
     neovim.enable = true;
     obsidian = {
       enable = true;
-      vaults.Notes = {
-        enable = true;
-        target = "Notes";
-      };
+      # vaults.Notes = {
+      #   enable = true;
+      #   target = "Notes";
+      # };
 
     };
     zathura.enable = true;
