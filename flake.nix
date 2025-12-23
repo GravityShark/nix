33721a1e8865c808ff2e6171078a25e13d23650f
Gravity <alternativegravitational@protonmail.com>
2025-12-23 22:23:31 +0800

    Update configuration

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
       "nix-command"
       "flakes"
     ];
-    extra-substitutors = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 
   inputs = {
