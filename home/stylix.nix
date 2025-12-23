6c109f121b3937abb58a0f5fb8f711464f2bb09b
Gravity <alternativegravitational@protonmail.com>
2025-12-23 23:36:20 +0800

    Update configuration

--- home/stylix.nix
+++ home/stylix.nix
@@ -77,12 +77,6 @@
   ## Niri
   xdg.configFile = {
     "niri/base16.kdl".text = with config.lib.stylix.colors.withHashtag; ''
-      output "eDP-1" {
-          layout {
-              background-color "${base00}"
-          }
-          backdrop-color "${base01}"
-      }
       layout {
           focus-ring {
               active-color   "${base0B}"
@@ -111,15 +105,31 @@
           }
       }
     '';
-    # "nvim/lua/base16.lua".text = with config.lib.stylix.colors.withHashtag; ''
-    #   return {
-    #                     base00 = '${base00}', base01 = '${base01}', base02 = '${base02}', base03 = '${base03}',
-    #                     base04 = '${base04}', base05 = '${base05}', base06 = '${base06}', base07 = '${base07}',
-    #                     base08 = '${base08}', base09 = '${base09}', base0A = '${base0A}', base0B = '${base0B}',
-    #                     base0C = '${base0C}', base0D = '${base0D}', base0E = '${base0E}', base0F = '${base0F}'
-    #                   }
-    # '';
+    "nvim/colors/base16.lua".text = with config.lib.stylix.colors.withHashtag; ''
+      require('mini.base16').setup({
+      	palette = {
+      		base00 = '${base00}',
+      		base01 = '${base01}',
+      		base02 = '${base02}',
+      		base03 = '${base03}',
+      		base04 = '${base04}',
+      		base05 = '${base05}',
+      		base06 = '${base06}',
+      		base07 = '${base07}',
+      		base08 = '${base08}',
+      		base09 = '${base09}',
+      		base0A = '${base0A}',
+      		base0B = '${base0B}',
+      		base0C = '${base0C}',
+      		base0D = '${base0D}',
+      		base0E = '${base0E}',
+      		base0F = '${base0F}',
+      	},
+      })
+      vim.g.colors_name = 'base16'
+    '';
   };
+
   ## Extra noctalia-shell
 
 }
