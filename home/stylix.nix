472c11741a006a2edfc95335d26f7209cae95247
Gravity <alternativegravitational@protonmail.com>
2025-12-24 14:02:03 +0800

    Update configuration

--- home/stylix.nix
+++ home/stylix.nix
@@ -107,7 +107,27 @@
     '';
     "nvim/colors/nix.lua".text =
       with config.lib.stylix.colors.withHashtag;
-      builtins.readFile ./dump/.config/nvim/colors/nix.lua;
+      pkgs.replaceVars {
+        src = ./dump/.config/nvim/colors/nix.lua;
+        replacements = {
+          base00 = "${base00}";
+          base01 = "${base01}";
+          base02 = "${base02}";
+          base03 = "${base03}";
+          base04 = "${base04}";
+          base05 = "${base05}";
+          base06 = "${base06}";
+          base07 = "${base07}";
+          base08 = "${base08}";
+          base09 = "${base09}";
+          base0A = "${base0A}";
+          base0B = "${base0B}";
+          base0C = "${base0C}";
+          base0D = "${base0D}";
+          base0E = "${base0E}";
+          base0F = "${base0F}";
+        };
+      };
   };
 
   ## Extra noctalia-shell
