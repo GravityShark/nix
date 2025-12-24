2eb074738b47e9dcfe824b47bdd450ffade2f595
Gravity <alternativegravitational@protonmail.com>
2025-12-24 17:05:20 +0800

    Update configuration

--- home/neovim.nix
+++ home/neovim.nix
@@ -4,9 +4,10 @@
   xdg.configFile = {
     "nvim/after".source = dump/.config/nvim/after;
     "nvim/enabled-plugins.txt".source = dump/.config/nvim/enabled-plugins.txt;
+    "nvim/relink.go".source = dump/.config/nvim/relink.go;
     "nvim/init.lua".source = dump/.config/nvim/init.lua;
     "nvim/snippets".source = dump/.config/nvim/snippets;
-    "nvim/lua/other".source = dump/.config/nvim/lua/other;
+    "nvim/lua/config".source = dump/.config/nvim/lua/other;
     "nvim/lua/pluggers".source = dump/.config/nvim/lua/pluggers;
     "nvim/lua/templates".source = dump/.config/nvim/lua/templates;
   };
