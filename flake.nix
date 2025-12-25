05c88a6588a0f05e200b048eccde738d5da90e70
Gravity <alternativegravitational@protonmail.com>
2025-12-26 01:27:06 +0800

    Update configuration

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v3.6.2";
+      url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
