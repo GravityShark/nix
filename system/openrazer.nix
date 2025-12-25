18a692441de0c8d99de53ef1fc17e61c9278c998
Gravity <alternativegravitational@protonmail.com>
2025-12-25 23:52:17 +0800

    Update configuration

---
+++ system/openrazer.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  hardware.openrazer.enable = true;
+  environment.systemPackages = with pkgs; [
+    openrazer-daemon
+    polychromatic
+  ];
+  users.users.gravity = {
+    extraGroups = [ "openrazer" ];
+  };
+}
