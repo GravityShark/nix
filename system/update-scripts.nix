f862661864c9f45078455b57b9be2b70058886a4
Gravity <alternativegravitational@protonmail.com>
2025-12-26 17:29:51 +0800

    Update configuration

--- system/update-scripts.nix
+++ system/update-scripts.nix
@@ -2,11 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    writeShellScriptBin
-    "my-script"
-    ''
+    (writeShellScriptBin "my-script" ''
       DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
       ${pkgs.cowsay}/bin/cowsay Hello, world! Today is $DATE.
-    ''
+    '')
   ];
 }
