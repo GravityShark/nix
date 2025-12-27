c9a49dfee53727c1146e91e74f35e8aa2ef8b39f
Gravity <alternativegravitational@protonmail.com>
2025-12-28 02:40:17 +0800

    Update configuration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "The WORST flake config you will EVER see";
 
   inputs = {
-    self.submodules = true;
+    # self.submodules = true;
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
