2557fbf9b569f10ef845b24db76d03a92e8ac54a
Gravity <alternativegravitational@protonmail.com>
2025-12-28 09:00:33 +0800

    Update configuration

--- hosts/msi/home.nix
+++ hosts/msi/home.nix
@@ -5,7 +5,7 @@
     stylix.enable = true;
     noctalia.enable = true;
   };
-  desktop-entries.enable = true;
+  # desktop-entries.enable = true;
 
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
