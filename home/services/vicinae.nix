b69f9da7b634b8dc7913606873c740d78ea0ea39
Gravity <alternativegravitational@protonmail.com>
2025-12-28 10:11:16 +0800

    Update configuration

--- home/services/vicinae.nix
+++ home/services/vicinae.nix
@@ -5,32 +5,32 @@
     service.vicinae.enable = lib.mkEnableOption "enables vicinae server to allow for start startup, and enables the binary for use";
   };
   config = lib.mkIf config.service.vicinae.enable {
-    services.vicinae = {
-      enable = true;
-      settings = {
-        closeOnFocusLoss = true;
-        considerPreedit = false;
-        faviconService = "twenty";
-        font = {
-          normal = "Aporetic Sans";
-          size = 11;
-        };
-        keybinding = "default";
-        keybinds = {
-        };
-        popToRootOnClose = true;
-        rootSearch = {
-          searchFiles = true;
-        };
-        theme = {
-          name = "matugen";
-        };
-        window = {
-          csd = false;
-          opacity = 1;
-          rounding = 10;
-        };
-      };
-    };
+    # services.vicinae = {
+    #   enable = true;
+    #   settings = {
+    #     closeOnFocusLoss = true;
+    #     considerPreedit = false;
+    #     faviconService = "twenty";
+    #     font = {
+    #       normal = "Aporetic Sans";
+    #       size = 11;
+    #     };
+    #     keybinding = "default";
+    #     keybinds = {
+    #     };
+    #     popToRootOnClose = true;
+    #     rootSearch = {
+    #       searchFiles = true;
+    #     };
+    #     theme = {
+    #       name = "matugen";
+    #     };
+    #     window = {
+    #       csd = false;
+    #       opacity = 1;
+    #       rounding = 10;
+    #     };
+    #   };
+    # };
   };
 }
