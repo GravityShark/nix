ae6686a0ac63c265e842a30b2954babe336af1da
Gravity <alternativegravitational@protonmail.com>
2025-12-31 19:31:06 +0800

    Update configuration

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -1,6 +1,6 @@
 # man `configuration.nix(5)` or `nixos-help`
 
-{ config, ... }:
+{ ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -12,7 +12,7 @@
   system = {
     doas.enable = true;
     intel.enable = true;
-    # msi.enable = true;
+    msi.enable = true;
     nvidia.enable = true;
     openrazer.enable = false;
     systemd-boot.enable = true;
@@ -37,16 +37,4 @@
 
   # `man configuration.nix` or https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11";
-
-  boot.extraModulePackages = with config.boot.kernelPackages; [ msi-ec ];
-  # boot.kernelModules = [ "msi-ec" ];
-  # Sets the msi stats
-  # systemd.tmpfiles.rules = [
-  #   "w /sys/devices/platform/msi-ec/webcam - - - - off"
-  #   "w /sys/devices/platform/msi-ec/shift_mode - - - - eco"
-  #   "w /sys/class/leds/msiacpi::kbd_backlight/brightness - - - - 0"
-  #   "w /sys/class/power_supply/BAT1/charge_control_start_threshold - - - - 70"
-  #   "w /sys/class/power_supply/BAT1/charge_control_end_threshold - - - - 00"
-  # ];
-
 }
--- nixos/system/msi.nix
+++ nixos/system/msi.nix
@@ -14,7 +14,7 @@
     # Sets the msi stats
     systemd.tmpfiles.rules = [
       "w /sys/devices/platform/msi-ec/webcam - - - - off"
-      "w /sys/devices/platform/msi-ec/shift_mode - - - - eco"
+      # "w /sys/devices/platform/msi-ec/shift_mode - - - - eco"
       "w /sys/class/leds/msiacpi::kbd_backlight/brightness - - - - 0"
       "w /sys/class/power_supply/BAT1/charge_control_start_threshold - - - - 70"
       "w /sys/class/power_supply/BAT1/charge_control_end_threshold - - - - 00"
