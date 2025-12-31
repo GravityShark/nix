bb7bc11ea6d131ed336eb9b12619358e8995809e
Gravity <alternativegravitational@protonmail.com>
2025-12-31 18:41:31 +0800

    Update configuration

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -1,6 +1,6 @@
 # man `configuration.nix(5)` or `nixos-help`
 
-{ ... }:
+{ config, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -38,5 +38,16 @@
 
   # `man configuration.nix` or https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11";
+
+  boot.extraModulePackages = [ config.boot.kernelPackages.msi-ec ];
+  boot.kernelModules = [ "msi-ec" ];
+  # Sets the msi stats
+  systemd.tmpfiles.rules = [
+    "w /sys/devices/platform/msi-ec/webcam - - - - off"
+    "w /sys/devices/platform/msi-ec/shift_mode - - - - eco"
+    "w /sys/class/leds/msiacpi::kbd_backlight/brightness - - - - 0"
+    "w /sys/class/power_supply/BAT1/charge_control_start_threshold - - - - 70"
+    "w /sys/class/power_supply/BAT1/charge_control_end_threshold - - - - 00"
+  ];
 
 }
