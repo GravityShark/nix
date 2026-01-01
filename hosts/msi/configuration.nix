0c806c3baddfef1ed3d883642d4cbb6a5f176ee0
Gravity <alternativegravitational@protonmail.com>
2026-01-02 00:26:32 +0800

    Update configuration

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -1,6 +1,6 @@
 # man `configuration.nix(5)` or `nixos-help`
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -35,20 +35,25 @@
   # Helpful scripts for updating and maintaining the system
   update-scripts.enable = true;
 
-  services.tuned.profiles = {
-    # balanced = {
-    #   script = {
-    #     "script" = "$\{i:PROFILE_DIR}/script.sh";
-    #     type = "script";
-    #   };
-    # };
-    # throughput-performance = {
-    #   script = {
-    #     "script" = "$\{i:PROFILE_DIR}/script.sh";
-    #     type = "script";
-    #   };
-    # };
-  };
+  # services.tuned.profiles = {
+  # balanced = {
+  #   script = {
+  #     "script" = "$\{i:PROFILE_DIR}/script.sh";
+  #     type = "script";
+  #   };
+  # };
+  # throughput-performance = {
+  #   script = {
+  #     "script" = "$\{i:PROFILE_DIR}/script.sh";
+  #     type = "script";
+  #   };
+  # };
+  # };
+
+  programs.adb.enable = true;
+  users.users.gravity.extraGroups = [ "adbusers" ];
+
+  environment.systemPackages = with pkgs; [ android-file-transfer ];
 
   # `man configuration.nix` or https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11";
