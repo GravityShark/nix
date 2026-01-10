e6bf57a009ce9bbeea533295cac71d030d87df7c
Gravity <alternativegravitational@protonmail.com>
2026-01-10 22:38:48 +0800

    Update configuration

--- home/apps/minecraft.nix
+++ home/apps/minecraft.nix
@@ -54,6 +54,15 @@
           ))
         ];
       }))
+      glfw3.overrideAttrs
+      (o: {
+        patches = o.patches ++ [
+          (fetchpatch {
+            url = "https://raw.githubusercontent.com/tesselslate/waywall/be3e018bb5f7c25610da73cc320233a26dfce948/contrib/glfw.patch";
+            hash = "sha256-8Sho5Yoj/FpV7utWz3aCXNvJKwwJ3ZA3qf1m2WNxm5M=";
+          })
+        ];
+      })
 
       # https://github.com/MarwinKreuzig/nixos-config/blob/17864a2c8995f2cb84a2454a27e23f158023ce32/modules/gaming/mcsr/packages/ninjabrainbot/default.nix
       (maven.buildMavenPackage rec {
