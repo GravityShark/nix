289dee2e6e79c8078dab8e40401f28cc267e986c
Gravity <alternativegravitational@protonmail.com>
2026-01-10 22:41:41 +0800

    Update configuration

--- home/apps/minecraft.nix
+++ home/apps/minecraft.nix
@@ -12,9 +12,6 @@
     apps.minecraft.enable = lib.mkEnableOption "enables minecraft";
   };
   config = lib.mkIf config.apps.minecraft.enable {
-    home.sessionVariables = {
-      LD_PRELOAD = "${pkgs.jemalloc}/lib/libjemalloc.so";
-    };
     home.packages = with pkgs; [
       lunar-client
       jdk21
@@ -54,14 +51,14 @@
           ))
         ];
       }))
-      (glfw3-minecraft.overrideAttrs (o: {
-        patches = o.patches ++ [
-          (fetchpatch {
-            url = "https://raw.githubusercontent.com/tesselslate/waywall/be3e018bb5f7c25610da73cc320233a26dfce948/contrib/glfw.patch";
-            hash = "sha256-8Sho5Yoj/FpV7utWz3aCXNvJKwwJ3ZA3qf1m2WNxm5M=";
-          })
-        ];
-      }))
+      # (glfw3-minecraft.overrideAttrs (o: {
+      #   patches = o.patches ++ [
+      #     (fetchpatch {
+      #       url = "https://raw.githubusercontent.com/tesselslate/waywall/be3e018bb5f7c25610da73cc320233a26dfce948/contrib/glfw.patch";
+      #       hash = "sha256-8Sho5Yoj/FpV7utWz3aCXNvJKwwJ3ZA3qf1m2WNxm5M=";
+      #     })
+      #   ];
+      # }))
 
       # https://github.com/MarwinKreuzig/nixos-config/blob/17864a2c8995f2cb84a2454a27e23f158023ce32/modules/gaming/mcsr/packages/ninjabrainbot/default.nix
       (maven.buildMavenPackage rec {
