7500234bdf6c307a03713038495d5bb5a3488d73
Gravity <alternativegravitational@protonmail.com>
2026-01-10 19:40:59 +0800

    Update configuration

--- home/apps/minecraft.nix
+++ home/apps/minecraft.nix
@@ -17,7 +17,7 @@
     };
     home.packages = with pkgs; [
       jemalloc
-      lunarclient
+      lunar-client
       # javaPackages.compiler.temurin-bin.jre-21
 
       (prismlauncher.override (previous: {
