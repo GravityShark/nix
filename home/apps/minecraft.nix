66c475a19c90b4bc1e56bae4880e7395d5870a02
Gravity <alternativegravitational@protonmail.com>
2026-01-10 19:39:42 +0800

    Update configuration

--- home/apps/minecraft.nix
+++ home/apps/minecraft.nix
@@ -38,63 +38,64 @@
           libxkbcommon
         ];
       }))
+
       # https://github.com/MarwinKreuzig/nixos-config/blob/17864a2c8995f2cb84a2454a27e23f158023ce32/modules/gaming/mcsr/packages/ninjabrainbot/default.nix
-      (pkgs.callPackage (maven.buildMavenPackage rec {
-        pname = "ninjabrainbot";
-        version = "1.5.1";
-        src = pkgs.fetchFromGitHub {
-          owner = "Ninjabrain1";
-          repo = "Ninjabrain-Bot";
-          rev = version;
-          hash = "sha256-r8TpL3VLf2QHwFS+DdjxgxyuZu167fP6/lN7a8e9opM=";
-        };
-        mvnHash = "sha256-zAVPH5a7ut21Ipz5BUY6cnRLT52bM8Yo2r8ClFon1p0";
-
-        desktopItems = [
-          (pkgs.makeDesktopItem {
-            name = "ninjabrainbot";
-            type = "Application";
-            exec = "ninjabrainbot";
-            comment = "An accurate stronghold calculator for Minecraft speedrunning.";
-            desktopName = "Ninjabrain Bot";
-            genericName = "A Minecraft stronghold calculator";
-            categories = [ "Game" ];
-          })
-        ];
-
-        mvnDepsParameters = "assembly:single -DskipTests=true";
-        mvnParameters = "assembly:single -DskipTests=true";
-
-        nativeBuildInputs = [
-          pkgs.copyDesktopItems
-          pkgs.makeWrapper
-        ];
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/bin $out/share/ninjabrainbot
-          install -Dm644 target/ninjabrainbot-${version}-jar-with-dependencies.jar $out/share/ninjabrainbot
-
-          makeWrapper ${pkgs.jre}/bin/java $out/bin/ninjabrainbot \
-            --prefix LD_LIBRARY_PATH : "${
-              pkgs.lib.makeLibraryPath (
-                with pkgs;
-                [
-                  libxkbcommon
-                  xorg.libX11
-                  xorg.libXt
-                  xorg.libXtst
-                  xorg.libXinerama
-                  xorg.libxcb
-                ]
-              )
-            }" \
-            --add-flags "-DSwing.aatext=TRUE -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dawt.useSystemAAFontSettings=on -jar $out/share/ninjabrainbot/ninjabrainbot-${version}-jar-with-dependencies.jar -Djava.util.prefs.userRoot=$HOME/.config/ninjabrainbot"
-
-          runHook postInstall
-        '';
-      }) { })
+      # (pkgs.callPackage (maven.buildMavenPackage rec {
+      #   pname = "ninjabrainbot";
+      #   version = "1.5.1";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "Ninjabrain1";
+      #     repo = "Ninjabrain-Bot";
+      #     rev = version;
+      #     hash = "sha256-r8TpL3VLf2QHwFS+DdjxgxyuZu167fP6/lN7a8e9opM=";
+      #   };
+      #   mvnHash = "sha256-zAVPH5a7ut21Ipz5BUY6cnRLT52bM8Yo2r8ClFon1p0";
+      #
+      #   desktopItems = [
+      #     (pkgs.makeDesktopItem {
+      #       name = "ninjabrainbot";
+      #       type = "Application";
+      #       exec = "ninjabrainbot";
+      #       comment = "An accurate stronghold calculator for Minecraft speedrunning.";
+      #       desktopName = "Ninjabrain Bot";
+      #       genericName = "A Minecraft stronghold calculator";
+      #       categories = [ "Game" ];
+      #     })
+      #   ];
+      #
+      #   mvnDepsParameters = "assembly:single -DskipTests=true";
+      #   mvnParameters = "assembly:single -DskipTests=true";
+      #
+      #   nativeBuildInputs = [
+      #     pkgs.copyDesktopItems
+      #     pkgs.makeWrapper
+      #   ];
+      #
+      #   installPhase = ''
+      #     runHook preInstall
+      #
+      #     mkdir -p $out/bin $out/share/ninjabrainbot
+      #     install -Dm644 target/ninjabrainbot-${version}-jar-with-dependencies.jar $out/share/ninjabrainbot
+      #
+      #     makeWrapper ${pkgs.jre}/bin/java $out/bin/ninjabrainbot \
+      #       --prefix LD_LIBRARY_PATH : "${
+      #         pkgs.lib.makeLibraryPath (
+      #           with pkgs;
+      #           [
+      #             libxkbcommon
+      #             xorg.libX11
+      #             xorg.libXt
+      #             xorg.libXtst
+      #             xorg.libXinerama
+      #             xorg.libxcb
+      #           ]
+      #         )
+      #       }" \
+      #       --add-flags "-DSwing.aatext=TRUE -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dawt.useSystemAAFontSettings=on -jar $out/share/ninjabrainbot/ninjabrainbot-${version}-jar-with-dependencies.jar -Djava.util.prefs.userRoot=$HOME/.config/ninjabrainbot"
+      #
+      #     runHook postInstall
+      #   '';
+      # }) { })
       (pkgs.waywall.overrideAttrs (
         finalAttrs: previousAttrs: {
           version = "0-unstable-2026-01-10";
