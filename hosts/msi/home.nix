8fadb9b558ad1b81c9af02c08a9b387b1acfd5d0
Gravity <alternativegravitational@protonmail.com>
2026-01-15 22:54:06 +0800

    Update configuration

--- hosts/msi/home.nix
+++ hosts/msi/home.nix
@@ -88,60 +88,60 @@
   programs = {
     anki = {
       enable = true;
-      reduceMotion = true;
-      sync = {
-        autoSync = true;
-        syncMedia = true;
-      };
-      spacebarRatesCard = false;
-      addons = with pkgs; [
-        ankiAddons.review-heatmap
-
-        (anki-utils.buildAnkiAddon (finalAttrs: {
-          pname = "ankitty";
-          version = "1.1.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "marvinruder";
-            repo = "ankitty";
-            rev = "v${finalAttrs.version}";
-            sparseCheckout = [ "src/ankitty" ];
-            hash = "sha256-ra7kv+4Fh3YvXW5+vylvhvTF+E0kn0Futy1TJ5ygrJw=";
-          };
-          sourceRoot = "${finalAttrs.src.name}/src/ankitty";
-        }))
-
-        (anki-utils.buildAnkiAddon (finalAttrs: {
-          pname = "anki-note-linker";
-          version = "0-unstable-2025-9-20";
-          src = pkgs.fetchFromGitHub {
-            owner = "gugutu";
-            repo = "Anki-Note-Linker";
-            rev = "cfd51d17cfd3b40d21a7670d8c79728c8e8e4488";
-            hash = "sha256-sB7SoblB8lXxiSftTAK101CdeEmqImpr862BIN/5SAY=";
-          };
-        }))
-
-        (anki-utils.buildAnkiAddon (finalAttrs: {
-          pname = "automatic-note-linker";
-          version = "0-unstable-2024-9-16";
-          src = pkgs.fetchFromGitHub {
-            owner = "rrzhang139";
-            repo = "automatic_note_linker";
-            rev = "114ae3593e338a95d6524aa5e3163746b36fbe56";
-            hash = "sha256-Xy5kZPOqVnLoPK/YzFGNE5iLBJbZ06b1nMUdtkhr27M=";
-          };
-        }))
-      ];
-      answerKeys = [
-        {
-          ease = 1;
-          key = "x";
-        }
-        {
-          ease = 3;
-          key = "c";
-        }
-      ];
+      # reduceMotion = true;
+      # sync = {
+      #   autoSync = true;
+      #   syncMedia = true;
+      # };
+      # spacebarRatesCard = false;
+      # addons = with pkgs; [
+      #   ankiAddons.review-heatmap
+      #
+      #   (anki-utils.buildAnkiAddon (finalAttrs: {
+      #     pname = "ankitty";
+      #     version = "1.1.1";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "marvinruder";
+      #       repo = "ankitty";
+      #       rev = "v${finalAttrs.version}";
+      #       sparseCheckout = [ "src/ankitty" ];
+      #       hash = "sha256-ra7kv+4Fh3YvXW5+vylvhvTF+E0kn0Futy1TJ5ygrJw=";
+      #     };
+      #     sourceRoot = "${finalAttrs.src.name}/src/ankitty";
+      #   }))
+      #
+      #   (anki-utils.buildAnkiAddon (finalAttrs: {
+      #     pname = "anki-note-linker";
+      #     version = "0-unstable-2025-9-20";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "gugutu";
+      #       repo = "Anki-Note-Linker";
+      #       rev = "cfd51d17cfd3b40d21a7670d8c79728c8e8e4488";
+      #       hash = "sha256-sB7SoblB8lXxiSftTAK101CdeEmqImpr862BIN/5SAY=";
+      #     };
+      #   }))
+      #
+      #   (anki-utils.buildAnkiAddon (finalAttrs: {
+      #     pname = "automatic-note-linker";
+      #     version = "0-unstable-2024-9-16";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "rrzhang139";
+      #       repo = "automatic_note_linker";
+      #       rev = "114ae3593e338a95d6524aa5e3163746b36fbe56";
+      #       hash = "sha256-Xy5kZPOqVnLoPK/YzFGNE5iLBJbZ06b1nMUdtkhr27M=";
+      #     };
+      #   }))
+      # ];
+      # answerKeys = [
+      #   {
+      #     ease = 1;
+      #     key = "x";
+      #   }
+      #   {
+      #     ease = 3;
+      #     key = "c";
+      #   }
+      # ];
     };
     vesktop.enable = true;
     zathura = {
