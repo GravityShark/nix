f72feff0977c29cd9e0443c4daebc9db51c5dfb7
Gravity <alternativegravitational@protonmail.com>
2026-01-18 15:58:19 +0800

    Update configuration

--- home/apps/packages/ninjabrainbot.nix
+++ home/apps/packages/ninjabrainbot.nix
@@ -1,6 +1,5 @@
 # https://github.com/MarwinKreuzig/nixos-config/blob/17864a2c8995f2cb84a2454a27e23f158023ce32/modules/gaming/mcsr/packages/ninjabrainbot/default.nix
 { pkgs }:
-
 pkgs.maven.buildMavenPackage rec {
   pname = "ninjabrainbot";
   version = "1.5.1";
@@ -10,7 +9,7 @@ pkgs.maven.buildMavenPackage rec {
     rev = version;
     hash = "sha256-r8TpL3VLf2QHwFS+DdjxgxyuZu167fP6/lN7a8e9opM=";
   };
-  mvnHash = "sha256-zAVPH5a7ut21Ipz5BUY6cnRLT52bM8Yo2r8ClFon1p0";
+  mvnHash = "sha256-y8OWAgn+/wKDumex+EIjyw2Cd5JS3Uc2uC4oh1pd1xY=";
 
   desktopItems = [
     (pkgs.makeDesktopItem {
