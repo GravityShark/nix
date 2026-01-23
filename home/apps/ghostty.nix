babfe1ccca1dc1acf69903319322ddefad8ad69b
Gravity <alternativegravitational@protonmail.com>
2026-01-24 04:01:36 +0800

    yeah

--- home/apps/ghostty.nix
+++ home/apps/ghostty.nix
@@ -28,7 +28,7 @@
         shell-integration = "fish";
         theme = lib.mkDefault "Gruvbox Material Light";
         window-decoration = "none";
-        keybind = "ctrl+enter";
+        keybind = "unbind:ctrl+enter";
       };
       systemd.enable = false;
     };
