8e6cde8324549229bd4c5abd3d1f0d6ba443a5cc
Gravity <alternativegravitational@protonmail.com>
2026-02-08 13:46:22 +0800

    Update configuration

--- nixos/service/flatpak.nix
+++ nixos/service/flatpak.nix
@@ -6,11 +6,11 @@
 }:
 
 {
+  imports = [ inputs.flatpaks.nixosModules.nix-flatpak ];
   options = {
     service.flatpak.enable = lib.mkEnableOption "enables flatpak";
   };
   config = lib.mkIf config.service.flatpak.enable {
-    imports = [ inputs.flatpaks.nixosModules.nix-flatpak ];
 
     ## Currently only Roblox works with flatpak
     services.flatpak = {
