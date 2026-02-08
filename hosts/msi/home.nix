a35aec92627a8cab338a9ffc8d92511f9f828545
Gravity <alternativegravitational@protonmail.com>
2026-02-08 13:40:22 +0800

    Update configuration

--- hosts/msi/home.nix
+++ hosts/msi/home.nix
@@ -86,7 +86,7 @@
     ## Gaming
     # dolphin-emu
     # rare
-    # (bottles-unwrapped.override { removeWarningPopup = true; })
+    (bottles-unwrapped.override { removeWarningPopup = true; })
     umu-launcher
     vkbasalt
   ];
