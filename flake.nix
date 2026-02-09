b2dcddcd177177c3b6235a59579c2aeb58eb005e
Gravity <alternativegravitational@protonmail.com>
2026-02-09 20:55:43 +0800

    Update configuration

--- flake.nix
+++ flake.nix
@@ -57,8 +57,6 @@
         };
       };
       homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-        useGlobalPkgs = true;
-        useUserPackages = true;
         extraSpecialArgs = { inherit inputs; };
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
