193c6d81770d386531e1b5cd3a6c57dde9ca4db9
Gravity <alternativegravitational@protonmail.com>
2026-02-12 10:50:04 +0800

    Update configuration

--- home/desktop/mango.nix
+++ home/desktop/mango.nix
@@ -27,9 +27,6 @@
 
     services.wl-clip-persist.enable = true;
 
-    # # xwayland dpi scale
-    # echo "Xft.dpi: 140" | xrdb -merge
-
     xdg.configFile."mango/config.conf".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/dump/.config/mango/config.conf";
   };
