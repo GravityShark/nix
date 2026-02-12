d456daadb63f6a8270698fe3daeee1d6ee8e9d9d
Gravity <alternativegravitational@protonmail.com>
2026-02-12 15:26:52 +0800

    Update configuration

--- nixos/desktop/niri.nix
+++ nixos/desktop/niri.nix
@@ -6,11 +6,9 @@
 }:
 
 {
-  config =
-    # autologin https://superuser.com/a/1904473
-    lib.mkIf (config.desktop.display-server == "niri") {
-      programs.niri.enable = true;
-      environment.systemPackages = [ pkgs.xwayland-satellite ];
-      config.desktop.command = "${pkgs.niri}/bin/niri-session";
-    };
+  config = lib.mkIf (config.desktop.display-server == "niri") {
+    programs.niri.enable = true;
+    environment.systemPackages = [ pkgs.xwayland-satellite ];
+    desktop.command = "${pkgs.niri}/bin/niri-session";
+  };
 }
