752935e0ef4e163855cb5190bab911d7d11dd48b
Gravity <alternativegravitational@protonmail.com>
2026-02-15 21:44:19 +0800

    Update configuration

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -1,6 +1,6 @@
 # man `configuration.nix(5)` or `nixos-help` or https://nixos.org/nixos/options.html).
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -56,15 +56,25 @@
   ## Use labwc instead when we are gaming cause it has tearing support
   ## we gotta wait for this https://github.com/niri-wm/niri/issues/844
   # programs.labwc.enable = true;
-  programs.nix-ld = {
-    enable = true;
-    # libraries = with pkgs; [
-    #   ## Put here any library that is required when running a package
-    #   ## ...
-    #   ## Uncomment if you want to use the libraries provided by default in the steam distribution
-    #   ## but this is quite far from being exhaustive
-    #   ## https://github.com/NixOS/nixpkgs/issues/354513
-    #   # (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
-    # ];
-  };
+
+  # programs.nix-ld = {
+  #   enable = true;
+  # libraries = with pkgs; [
+  #   ## Put here any library that is required when running a package
+  #   ## ...
+  #   ## Uncomment if you want to use the libraries provided by default in the steam distribution
+  #   ## but this is quite far from being exhaustive
+  #   ## https://github.com/NixOS/nixpkgs/issues/354513
+  #   # (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+  # ];
+  # };
+
+  environment.systemPackages = with pkgs; [
+
+    gparted
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-base
+    gst_all_1.gstreamer
+    libmpg123
+  ];
 }
