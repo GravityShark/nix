8498c23135076639d2b87312c3c61973e420df44
Gravity <alternativegravitational@protonmail.com>
2026-02-23 05:35:08 +0800

    Update configuration

--- nixos/system/msi.nix
+++ nixos/system/msi.nix
@@ -66,6 +66,8 @@
             ${write (gpu_temp + 3) 70}
             ${write (gpu_temp + 4) 80}
             ${write (gpu_temp + 5) 90}
+
+            echo advanced > /sys/devices/platform/msi-ec/fan_mode
             echo "Successful."
           ''}";
         Restart = "on-failure";
@@ -78,8 +80,6 @@
       "w /sys/class/leds/msiacpi::kbd_backlight/brightness - - - - 0"
       "w /sys/class/power_supply/BAT1/charge_control_end_threshold - - - - 60"
       "w /sys/class/power_supply/BAT1/charge_control_start_threshold - - - - 50"
-      "w /sys/devices/platform/msi-ec/fan_mode - - - - advanced"
-      # "w /sys/devices/platform/msi-ec/shift_mode - - - - eco"
       "w /sys/devices/platform/msi-ec/webcam - - - - off"
     ];
 
@@ -88,10 +88,7 @@
     systemd.services.ppd-dbus-hook = lib.mkIf config.service.power-management.enable {
       description = "Set /msi-ec/shift_mode depending on power-profiles-daemon";
       enable = true;
-      requires = [
-        "tuned-ppd.service"
-        "tlp-pd.service"
-      ];
+      requires = [ "tlp-pd.service" ];
       serviceConfig = {
         Type = "oneshot";
         ExecStart = ''
@@ -102,10 +99,7 @@
         '';
         Restart = "on-failure";
       };
-      wantedBy = [
-        "tuned-ppd.service"
-        "tlp-pd.service"
-      ];
+      wantedBy = [ "tlp-pd.service" ];
     };
   };
 }
