b6f16d2a1772ce3920133e39b6c3d9d791ffb015
Gravity <alternativegravitational@protonmail.com>
2026-03-07 14:37:50 +0800

    Update configuration

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
     flatpaks.url = "github:gmodena/nix-flatpak/latest";
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v4.6.1";
+      url = "github:noctalia-dev/noctalia-shell/v4.6.4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ppd-dbus-hook = {
