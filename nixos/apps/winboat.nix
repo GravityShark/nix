6970872abd24214be973e96aef4af79d08bdaf11
Gravity <alternativegravitational@protonmail.com>
2026-03-09 09:16:54 +0800

    Update configuration

--- nixos/apps/winboat.nix
+++ nixos/apps/winboat.nix
@@ -10,8 +10,16 @@
     apps.winboat.enable = lib.mkEnableOption "enables winboat";
   };
   config = lib.mkIf config.apps.winboat.enable {
-    virtualisation.docker.enable = true;
-    users.users.${config.username}.extraGroups = [ "docker" ];
+    # virtualisation.containers.enable = true;
+    virtualisation.podman.enable = true;
+    # virtualisation = {
+    #   podman = {
+    #     enable = true;
+    #     # Required for containers under podman-compose to be able to talk to each other.
+    #     # defaultNetwork.settings.dns_enabled = true;
+    #   };
+    # };
+    # users.users.${config.username}.extraGroups = [ "podman" ];
     environment.systemPackages = with pkgs; [ winboat ];
   };
 }
