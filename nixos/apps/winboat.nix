83cd4158c4e585d66d1765af7a7d0301d970d54f
Gravity <alternativegravitational@protonmail.com>
2026-03-13 06:38:26 +0800

    Update configuration

--- nixos/apps/winboat.nix
+++ nixos/apps/winboat.nix
@@ -10,30 +10,14 @@
     apps.winboat.enable = lib.mkEnableOption "enables winboat";
   };
   config = lib.mkIf config.apps.winboat.enable {
-    # virtualisation.docker.enable = true;
+    virtualisation.docker.enable = true;
+    users.users.${config.username}.extraGroups = [ "docker" ];
 
-    virtualisation.docker = {
-      # Consider disabling the system wide Docker daemon
-      enable = false;
-
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-        # Optionally customize rootless Docker daemon settings
-        daemon.settings = {
-          dns = [
-            "1.1.1.1"
-            "8.8.8.8"
-          ];
-          registry-mirrors = [ "https://mirror.gcr.io" ];
-        };
-      };
-    };
-    # users.users.${config.username}.extraGroups = [ "docker" ];
     # virtualisation.containers.enable = true;
     # virtualisation.podman.enable = true;
     # virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
     # users.users.${config.username}.extraGroups = [ "podman" ];
+
     environment.systemPackages = with pkgs; [ winboat ];
   };
 }
