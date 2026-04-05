19464ff912477a2e868589a4101eba0173146054
Gravity <alternativegravitational@protonmail.com>
2026-04-05 17:17:43 +0800

    Update configuration

--- dump/.config/sesh/scripts/coollittlewebsite.sh
+++ dump/.config/sesh/scripts/coollittlewebsite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 tmux split-window -v -l 5
-tmux send-keys "nix-shell --command \"cd scripts/; ./tailwindcss.sh --watch\"" Enter
+tmux send-keys "nix-shell --command \"cd web/scripts/; ./tailwindcss.sh --watch\"" Enter
 tmux select-pane -t :.+
 tmux send-keys "nix-shell --command \"air\"" Enter
 tmux new-window
