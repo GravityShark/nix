beca4e48b17a3231461bafdb95b401408b6d46f3
Gravity <alternativegravitational@protonmail.com>
2026-04-08 14:39:48 +0800

    Update configuration

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -47,6 +47,7 @@
   # Helpful scripts for updating and maintaining the system
   update-scripts.enable = true;
 
+  # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.11";
 
   ################################## Extra ###################################
