34781c6a0abb95df7ceb4f08b536697ddc7f63f8
Gravity <alternativegravitational@protonmail.com>
2026-04-09 13:55:02 +0800

    Update configuration

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -24,7 +24,7 @@
     doas.enable = true;
     drag-click.enable = true;
     intel.enable = true;
-    lanzaboote = true;
+    lanzaboote.enable = true;
     logind.enable = true;
     msi.enable = true;
     nvidia.enable = true;
