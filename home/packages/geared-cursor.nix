a3d8deba82c0e5812b4298229171f355c14e7d37
Gravity <alternativegravitational@protonmail.com>
2026-05-09 11:33:21 +0800

    Update configuration

--- home/packages/geared-cursor.nix
+++ home/packages/geared-cursor.nix
@@ -12,14 +12,19 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   sourceRoot = ".";
   src = fetchurl {
     url = "https://github.com/piraker-grinor/geared-cursor/releases/download/v${finalAttrs.version}/Geared-${finalAttrs.version}.7z";
-    name = "Geared-${finalAttrs.version}.7z";
+    # name = "Geared-${finalAttrs.version}.7z";
     hash = "sha256-muDO6hNyArC1v4eKryzoE1HIu5WyOsPX+NlR0S+oXk4=";
   };
 
-  unpackPhase = ":";
+  nativeBuildInputs = [ _7zz ];
+  unpackPhase = "
+    runHook preUnpacck 
+    7zz x $src -oGeared
+    runHook postUnpack
+  ";
+
   installPhase = ''
     mkdir -p $out/share/icons
-    ${_7zz}/bin/7zz x Geared-${finalAttrs.version}.7z -oGeared
     cp -r Geared $out/share/icons
   '';
 
