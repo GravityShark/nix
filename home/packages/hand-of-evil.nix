32d7bf3a2b53ef2d100b831f764fc15cddd538b1
Gravity <alternativegravitational@protonmail.com>
2026-05-09 09:54:32 +0800

    Update configuration

--- home/packages/hand-of-evil.nix
+++ home/packages/hand-of-evil.nix
@@ -8,17 +8,15 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "hand-of-evil";
   version = "1.2";
 
-  srcs = map (
-    color:
-    (fetchzip {
-      url = "https://github.com/Grief/hand-of-evil/releases/download/v${finalAttrs.version}/hand-of-evil.tar.gz";
-      name = "hand-of-evil";
-      hash = "sha256-K0P6EqlJEPke6IMi2viVDzdzHG4zoRv87u5dL5dMem0=";
-    })
-  );
+  sourceRoot = ".";
+  srcs = fetchzip {
+    url = "https://github.com/Grief/hand-of-evil/releases/download/v${finalAttrs.version}/hand-of-evil.tar.gz";
+    name = "hand-of-evil";
+    hash = "sha256-K0P6EqlJEPke6IMi2viVDzdzHG4zoRv87u5dL5dMem0=";
+  };
 
-  postInstall = ''
-    mkdir -p $out/share/icons
+  installPhase = ''
+    install -dm 0755 $out/share/icons
     cp -r hand-of-evil $out/share/icons
   '';
 
