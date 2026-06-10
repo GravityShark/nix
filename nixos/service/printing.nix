b9ad69e58ceab589480eb1adcedff6de05138545
Gravity <alternativegravitational@protonmail.com>
2026-06-10 14:46:43 +0800

    Update configuration

--- nixos/service/printing.nix
+++ nixos/service/printing.nix
@@ -10,24 +10,31 @@
     service.printing.enable = lib.mkEnableOption "enables printing";
   };
   config = lib.mkIf config.service.printing.enable {
+    services.ipp-usb.enable = true;
+    # https://wiki.nixos.org/wiki/Scanners
+    hardware.sane.enable = true;
+    users.users.${config.username}.extraGroups = [
+      "lp"
+      "scanner"
+    ];
     # https://nixos.wiki/wiki/Printing
     services.printing = {
       enable = true;
       drivers = with pkgs; [
         cups-filters
         cups-browsed
-        brlaser
-        brgenml1lpr
-        brgenml1cupswrapper
       ];
     };
+    environment.systemPackages = with pkgs; [
+      system-config-printer
+      sane-frontends
+      scanservjs
+    ];
 
     services.avahi = {
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
     };
-
-    services.ipp-usb.enable = true;
   };
 }
