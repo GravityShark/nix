8caf1a26231ded4284bc680663764fc2212d7112
Gravity <alternativegravitational@protonmail.com>
2026-06-23 10:01:00 +0800

    Update configuration

--- nixos/system/nvidia.nix
+++ nixos/system/nvidia.nix
@@ -20,25 +20,34 @@
       "modesetting"
     ];
 
-    # https://wiki.archlinux.org/title/PRIME#NVIDIA
-    # https://wiki.archlinux.org/title/NVIDIA/Tips_and_tricks#Preserve_video_memory_after_suspend
-    boot.kernelParams = [
-      "nvidia.NVreg_EnableGpuFirmware=0"
-      "nvidia.NVreg_UseKernelSuspendNotifiers=1"
-      "nvidia.NVreg_TemporaryFilePath=/var/tmp"
-    ];
-
     hardware.nvidia = {
+      hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.bleeding_edge;
+
       # Open drivers prevent going into D3
       # https://bbs.archlinux.org/viewtopic.php?pid=2187680#p2187680
-      open = false;
+      open = true;
 
       modesetting.enable = true;
+
+      # https://wiki.archlinux.org/title/PRIME#NVIDIA
+      # https://wiki.archlinux.org/title/NVIDIA/Tips_and_tricks#Preserve_video_memory_after_suspend
+      moduleParams = {
+        nvidia = {
+          NVreg_EnableGpuFirmware = 0;
+          NVreg_TemporaryFilePath = "/var/tmp";
+          NVreg_PreserveVideoMemoryAllocations = 0;
+        };
+      };
 
       # dynamicBoost.enable = true;
 
       powerManagement = {
+        # nvidia.NVreg_UseKernelSuspendNotifiers = 1;
+        #   }
+        #   ++ lib.optional cfg.powerManagement.enable { nvidia.NVreg_PreserveVideoMemoryAllocations = 1; }
         enable = true;
+        # cfg.powerManagement.finegrained { nvidia.NVreg_DynamicPowerManagement = "0x02"; }
+        # feingrained also adds the udev rule
         finegrained = true;
       };
 
