a0332b53233e22a84c14d6da7fe29f2b7d200ec1
Gravity <alternativegravitational@protonmail.com>
2026-08-05 12:17:10 +0800

    Update configuration

--- nixos/service/llama-cpp.nix
+++ nixos/service/llama-cpp.nix
@@ -21,44 +21,66 @@
         api-key = "bingo";
         ctx-size = 16384;
         # BUG: I think mcp servers are broken  https://github.com/ggml-org/llama.cpp/issues/26497
-        # mcp-servers-config = (pkgs.formats.json { }).generate "mcp.json" {
-        #   mcpServers = {
-        #     free-search-mcp = {
-        #       command = "${pkgs.uv}/bin/uvx";
-        #       args = [
-        #         "free-search-mcp"
-        #       ];
-        #     };
-        #     nixos = {
-        #       type = "stdio";
-        #       command = "nix";
-        #       args = [
-        #         "run"
-        #         "github:utensils/mcp-nixos"
-        #         "--"
-        #       ];
-        #     };
-        #   };
-        # };
+        mcp-servers-config = (pkgs.formats.json { }).generate "mcp.json" {
+          mcpServers = {
+            free-search-mcp = {
+              command = "${pkgs.uv}/bin/uvx";
+              args = [
+                "free-search-mcp"
+              ];
+            };
+            nixos = {
+              type = "stdio";
+              command = "nix";
+              args = [
+                "run"
+                "github:utensils/mcp-nixos"
+                "--"
+              ];
+            };
+          };
+        };
         models-autoload = "";
         models-max = 1;
         models-preset = (pkgs.formats.ini { }).generate "models-preset.ini" {
-          "unsloth/gemma-4-E4B-it-qat-UD-Q4_K_XL" = {
-            hf-repo = "unsloth/gemma-4-E4B-it-qat-GGUF";
-            hf-file = "gemma-4-E4B-it-qat-UD-Q4_K_XL.gguf";
-            n-gpu-layers = -1;
+          "unsloth/gemma-4-E4B-it-Q8_K_XL" = {
+            hf-repo = "unsloth/gemma-4-E4B-it-GGUF:UD-Q8_K_XL";
+            top-k = 64;
           };
-          "unsloth/gemma-4-E4B-it-UD-Q8_K_XL" = {
-            hf-repo = "unsloth/gemma-4-E4B-it-GGUF";
-            hf-file = "gemma-4-E4B-it-UD-Q8_K_XL.gguf";
-            n-gpu-layers = -1;
+          "unsloth/gemma-4-E2B-it-qat-UD-Q4_K_XL" = {
+            hf-repo = "unsloth/gemma-4-E2B-it-qat-GGUF:UD-Q4_K_XL";
+            top-k = 64;
+          };
+          "unsloth/gemma-4-E4B-it-qat-UD-Q4_K_XL" = {
+            hf-repo = "unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL";
+            top-k = 64;
+          };
+          "unsloth/gemma-4-12B-it-qat-Q4_K_XL" = {
+            hf-repo = "unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL";
+            top-k = 64;
+          };
+          "unsloth/gemma-4-26B-A4B-it-qat-Q4_K_XL" = {
+            hf-repo = "unsloth/gemma-4-26B-A4B-it-qat:UD-Q4_K_XL";
+            n-gpu-layers = "auto";
+            top-k = 64;
+          };
+          "unsloth/Qwen3.5-9B-Q4_K_XL" = {
+            hf-repo = "unsloth/Qwen3.5-9B-GGUF:UD-Q4_K_XL";
+            top-k = 20;
+            min-p = 0.00;
           };
         };
-        # some models crash if you dont disable the other stuff
+
+        n-gpu-layers = -1;
+        # some models crash if you dont disable this
         no-mmproj = "";
         parallel = 1;
         sleep-idle-seconds = 900;
+        spec-draft-n-max = 2;
+        spec-type = "draft-mtp";
+        temp = 1.0;
         tools = "all";
+        top-p = 0.95;
         ui-mcp-proxy = "";
         verbosity = 5;
       };
