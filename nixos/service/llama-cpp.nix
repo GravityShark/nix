e256eedf3d2d9b9ded2262066b8dbfbd7df95fa6
Gravity <alternativegravitational@protonmail.com>
2026-08-07 15:34:57 +0800

    Update configuration

--- nixos/service/llama-cpp.nix
+++ nixos/service/llama-cpp.nix
@@ -30,62 +30,82 @@
         settings = {
           api-key = "bingo";
           models-max = 1;
+          no-kv-offload = "";
           no-mmproj-offload = "";
           port = 9931;
-          ui-mcp-proxy = "";
+          # ui-mcp-proxy = "";
           verbosity = 5;
           models-preset = (pkgs.formats.ini { }).generate "models-preset.ini" {
             "*" = {
-              fit-ctx = 16384;
+              # fit-ctx = 16384;
               # fit-ctx = 32768;
               # fit-ctx = 65536;
               # ctx-size = 32768;
               # ctx-size = 65536;
               # ctx-size = 131072;
-              # ubatch-size = 2048;
-              # fit-target = "1024,0";
-              fit-target = "512";
+
+              no-kv-offload = "true";
+              batch-size = 2048;
+              ubatch-size = 2048;
+              fit-target = "0";
               n-gpu-layers = "all";
               parallel = 1;
-              # device = "none";
-              sleep-idle-seconds = 900;
+              device = "CUDA0";
+              sleep-idle-seconds = 300;
               temp = 1.0;
               top-k = 64;
               top-p = 0.95;
             };
             "Main/gemma-4-E2B-it-qat-Q4_K_XL" = {
-              alias = "Gemma 4 E2B";
+              hf-repo = "unsloth/gemma-4-E2B-it-qat-GGUF:UD-Q4_K_XL";
+            };
+            "Main/gemma-4-E2B-it-MTP-qat-Q4_K_XL" = {
               hf-repo = "unsloth/gemma-4-E2B-it-qat-GGUF:UD-Q4_K_XL";
               spec-draft-n-max = 2;
               spec-type = "draft-mtp";
             };
             "Main/gemma-4-E4B-it-qat-Q4_K_XL" = {
-              alias = "Gemma 4 E4B";
+              hf-repo = "unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL";
+            };
+            "Main/gemma-4-E4B-it-MTP-qat-Q4_K_XL" = {
               hf-repo = "unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL";
               spec-draft-n-max = 2;
               spec-type = "draft-mtp";
             };
-            "Main/fun" = {
-              alias = "fun";
-              hf-repo = "huihui-ai/Huihui-gemma-4-E4B-it-qat-q4_0-unquantized-abliterated-GGUF";
-              spec-draft-n-max = 2;
-              spec-type = "draft-mtp";
-            };
-            "Main/gemma-4-12B-it-qat-Q4_K_XL" = {
-              alias = "Gemma 4 12B";
-              hf-repo = "unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL";
-              spec-draft-n-max = 2;
-              spec-type = "draft-mtp";
-            };
-            # "Main/gemma-4-26B-A4B-it-qat-Q4_K_XL" = {
-            #   alias = "Gemma 4 26B A4B";
-            #   hf-repo = "unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL";
-            #   n-gpu-layers = "auto";
+            # "Main/fun" = {
+            #   alias = "fun";
+            #   hf-repo = "huihui-ai/Huihui-gemma-4-E4B-it-qat-q4_0-unquantized-abliterated-GGUF";
             # };
-            #
+            # "Main/gemma-4-12B-it-qat-Q4_K_XL" = {
+            #   alias = "Gemma 4 12B";
+            #   hf-repo = "unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL";
+            #   spec-draft-n-max = 2;
+            #   spec-type = "draft-mtp";
+            # };
+            "Main/gemma-4-26B-A4B-it-qat-Q4_K_XL" = {
+              alias = "Gemma 4 26B A4B";
+              hf-repo = "unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL";
+              n-gpu-layers = "auto";
+              kv-offload = "true";
+            };
+            "Main/Qwen3.5-2B-Q4_K_XL" = {
+              hf-repo = "unsloth/Qwen3.5-2B-GGUF:UD-Q4_K_XL";
+              min-p = 0.00;
+              top-k = 20;
+              cache-type-k = "q8_0";
+              cache-type-v = "q8_0";
+            };
             "Main/Qwen3.5-2B-MTP-Q4_K_XL" = {
-              alias = "Qwen 3.5 2B";
               hf-repo = "unsloth/Qwen3.5-2B-MTP-GGUF:UD-Q4_K_XL";
+              min-p = 0.00;
+              top-k = 20;
+              cache-type-k = "q8_0";
+              cache-type-v = "q8_0";
+              spec-draft-n-max = 2;
+              spec-type = "draft-mtp";
+            };
+            "Main/Qwen3.5-4B-Q4_K_XL" = {
+              hf-repo = "unsloth/Qwen3.5-4B-GGUF:UD-Q4_K_XL";
               min-p = 0.00;
               top-k = 20;
               cache-type-k = "q8_0";
@@ -93,24 +113,25 @@
               # load-on-startup = "true";
             };
             "Main/Qwen3.5-4B-MTP-Q4_K_XL" = {
-              alias = "Qwen 3.5 4B";
               hf-repo = "unsloth/Qwen3.5-4B-MTP-GGUF:UD-Q4_K_XL";
               min-p = 0.00;
               top-k = 20;
-              cache-type-k = "q4_0";
-              cache-type-v = "q4_0";
-            };
-            "Main/Qwen3.5-9B-MTP-Q4_K_XL" = {
-              alias = "Qwen 3.5 9B";
-              hf-repo = "unsloth/Qwen3.5-9B-MTP-GGUF:UD-Q4_K_XL";
-              min-p = 0.00;
-              top-k = 20;
-              cache-type-k = "q4_0";
-              cache-type-v = "q4_0";
-              n-gpu-layers = 15;
+              cache-type-k = "q8_0";
+              cache-type-v = "q8_0";
               spec-draft-n-max = 2;
               spec-type = "draft-mtp";
             };
+            # "Main/Qwen3.5-9B-MTP-Q4_K_XL" = {
+            #   alias = "Qwen 3.5 9B";
+            #   hf-repo = "unsloth/Qwen3.5-9B-MTP-GGUF:UD-Q4_K_XL";
+            #   min-p = 0.00;
+            #   top-k = 20;
+            #   cache-type-k = "q4_0";
+            #   cache-type-v = "q4_0";
+            #   n-gpu-layers = 15;
+            #   spec-draft-n-max = 2;
+            #   spec-type = "draft-mtp";
+            # };
             # "unsloth/Qwen3.6-27B-Q4_K_XL" = {
             #   alias = "Qwen 3.6 27B";
             #   hf-repo = "unsloth/Qwen3.6-27B-GGUF:UD-Q4_K_XL";
@@ -119,13 +140,14 @@
             #   cache-type-k = "q8_0";
             #   cache-type-v = "q8_0";
             # };
-            # "unsloth/Qwen3.6-35B-A3B-Q4_K_XL" = {
-            #   alias = "Qwen 3.6 35B A3B";
-            #   hf-repo = "unsloth/Qwen3.6-35B-A3B-GGUF:UD-Q4_K_XL";
-            #   min-p = 0.00;
-            #   top-k = 20;
-            #   n-gpu-layers = "auto";
-            # };
+            "Main/Qwen3.6-35B-A3B-IQ4_XS" = {
+              alias = "Qwen 3.6 35B A3B";
+              hf-repo = "unsloth/Qwen3.6-35B-A3B-GGUF:UD-IQ4_XS";
+              min-p = 0.00;
+              top-k = 20;
+              n-gpu-layers = "auto";
+              kv-offload = "true";
+            };
           };
         };
       };
